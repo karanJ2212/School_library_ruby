70343d6c4ee8b1c672c6b27eff51927fa24ca5c3
karan jain <karanjain2212@gmail.com>
2023-05-26 20:32:21 +0530

    correction in main.rb

--- main.rb
+++ main.rb
@@ -4,6 +4,6 @@ require_relative 'trimmer_decorator'
 person = Person.new(22, name: 'maximilianus')
 p " original name -#{person.correct_name}"
 Capitalized_person = CapitalizeDecorator.new(person)
-p "capitalized: #{capitalizedPerson.correct_name}"
-Capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
-p "trimmed - #{capitalizedTrimmedPerson.correct_name}"
+p "capitalized: #{Capitalized_person.correct_name}"
+Capitalized_trimmed_person = TrimmerDecorator.new(Capitalized_person)
+p "trimmed - #{Capitalized_trimmed_person.correct_name}"
