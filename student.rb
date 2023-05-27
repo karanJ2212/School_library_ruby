feb65f14c1a636c299562489c2bcab097d6bf24d
karan jain <karanjain2212@gmail.com>
2023-05-27 13:19:15 +0530

    implemented associations

--- student.rb
+++ student.rb
@@ -1,8 +1,11 @@
 require_relative 'person'
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, parent_permission, name)
     @classroom = classroom
+    classroom&.add_student(self)
   end
 
   def play_hooky
