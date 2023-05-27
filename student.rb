8ced0eda0e9d6ce401e299961e1d58b94e1967f9
karan jain <karanjain2212@gmail.com>
2023-05-27 18:41:23 +0530

    fixed linters

--- student.rb
+++ student.rb
@@ -6,12 +6,12 @@ class Student < Person
   def initialize(age, name, parent_permission: true, classroom: nil)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-  
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
+
   def add_classroom(classroom)
     @classroom = classroom
     classroom.add_student(self) unless classroom.students.include?(self)
