00438146e82c14488c84108c2530ddaf91597698
karan jain <karanjain2212@gmail.com>
2023-05-27 14:33:38 +0530

    created app and main

--- main.rb
+++ main.rb
@@ -1,34 +1,112 @@
-require_relative 'person'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
-require_relative 'classroom'
+require_relative 'app'
 
-# Create a Person object
-person = Person.new(22, name: 'maximilianus')
-puts "Person Name: #{person.correct_name}"
+def main
+  app = App.new
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts "Capitalized Person Name: #{capitalized_person.correct_name}"
+  loop do
+    display_menu
 
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts "Capitalized and Trimmed Person Name: #{capitalized_trimmed_person.correct_name}"
+    choice = gets.chomp.to_i
 
-# Create a Classroom object
-classroom = Classroom.new('Classroom A')
+    handle_choice(choice, app)
 
-# Student object and add it to the classroom
-student = Student.new(16, classroom)
-puts "Student ID: #{student.id}"
-puts "Student Name: #{student.name}"
-puts "Student Age: #{student.age}"
-puts "Can Use Services? #{student.can_use_services?}"
-puts student.play_hooky
+    break if choice == 7
+  end
 
-#  Teacher object
-teacher = Teacher.new(40, 'Math')
-puts "Teacher ID: #{teacher.id}"
-puts "Teacher Name: #{teacher.name}"
-puts "Teacher Age: #{teacher.age}"
-puts "Can Use Services? #{teacher.can_use_services?}"
+  puts 'Goodbye!'
+end
+
+def display_menu
+  puts 'Welcome to the Rental App!'
+  puts 'Please choose an option:'
+  puts '1. List all books'
+  puts '2. List all people'
+  puts '3. Create a person'
+  puts '4. Create a book'
+  puts '5. Create a rental'
+  puts '6. List rentals for a person'
+  puts '7. Quit'
+end
+
+def handle_choice(choice, app)
+  options = {
+    1 => :list_all_books,
+    2 => :list_all_people,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals_for_person,
+    7 => :quit
+  }
+
+  if options.key?(choice)
+    send(options[choice], app)
+  else
+    puts 'Invalid choice. Please try again.'
+  end
+end
+
+def list_all_books(app)
+  app.list_all_books
+end
+
+def list_all_people(app)
+  app.list_all_people
+end
+
+def create_person(app)
+  puts "Enter the person's name:"
+  name = gets.chomp
+
+  puts "Enter the person's age:"
+  age = gets.chomp.to_i
+
+  puts 'Do you want to create a student(1) or a teacher(2)? [input the number]'
+  type = gets.chomp
+
+  if type == '2'
+    app.create_person(name, age, 'teacher')
+  elsif type == '1'
+    puts "Enter the student's classroom:"
+    classroom = gets.chomp
+    app.create_person(name, age, 'student', classroom)
+  else
+    puts 'Invalid person type!'
+  end
+end
+
+def create_book(app)
+  puts "Enter the book's title:"
+  title = gets.chomp
+
+  puts "Enter the book's author:"
+  author = gets.chomp
+
+  app.create_book(title, author)
+end
+
+def create_rental(app)
+  puts "Enter the person's ID:"
+  person_id = gets.chomp.to_i
+
+  puts "Enter the book's title:"
+  book_title = gets.chomp
+
+  puts 'Enter the rental date (YYYY-MM-DD):'
+  rental_date = gets.chomp
+
+  app.create_rental(person_id, book_title, rental_date)
+end
+
+def list_rentals_for_person(app)
+  puts "Enter the person's ID:"
+  person_id = gets.chomp.to_i
+
+  app.list_rentals_for_person(person_id)
+end
+
+def quit(_app)
+  # No action needed
+end
+# Invoke the entry point
+main
