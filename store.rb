d3ef71bd614f241dd7ce6d613b96bb0305098c66
karan jain <karanjain2212@gmail.com>
2023-05-30 13:52:51 +0530

    testing storage

--- store.rb
+++ store.rb
@@ -9,20 +9,17 @@ class Store
     @rentals = []
   end
 
-
   def store_people(file, data)
     @people = @handle_data.read_data(file) if File.exist?(file)
     @people.push(data)
     @handle_data.save_data(file, @people)
   end
 
-
   def store_books(file, data)
     @books = @handle_data.read_data(file) if File.exist?(file)
     @books.push(data)
     @handle_data.save_data(file, @books)
   end
-
 
   def store_rentals(file, data)
     @rentals = @handle_data.read_data(file) if File.exist?(file)
