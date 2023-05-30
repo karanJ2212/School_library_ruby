d2f6dd122b68c434fc0e9a35b6ff8a9f2956e84d
karan jain <karanjain2212@gmail.com>
2023-05-30 13:39:59 +0530

    created preserve_data

---
+++ preserve_data.rb
@@ -0,0 +1,13 @@
+require 'json'
+
+
+class HandleData
+  def read_data(filename)
+    File.empty?(filename) ? [] : JSON.parse(File.read(filename))
+  end
+
+
+  def save_data(filename, data)
+    File.write(filename, JSON.generate(data))
+  end
+end
